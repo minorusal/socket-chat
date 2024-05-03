aff9c13e1ef4fe33046c4ee574ece3d468267364
minorusal <minorusal@hotmail.com>
2024-05-02 21:29:01 -0600

    Se genera nuevo socket con origen  especifico

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 const { escucharSockets } = require('./utils/server');
 
 const app = express();
